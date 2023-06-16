3dd28c40cee18da2a07ebe94c8503184c7b6ddb9
大陸 永井 <k022g0053@m.neec.ac.jp>
2023-06-17 01:56:58 +0900

    Half Lambert 実装

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -30,7 +30,8 @@ PixelShaderOutput main(VertexShaderOutput input) {
 	// ランバート反射モデルを利用した Lighting の計算
 	if (gMaterial.enableLighting != 0) {
 		// Lighting する場合
-		float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+		float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+		float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
 		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
 	}
 	else {
